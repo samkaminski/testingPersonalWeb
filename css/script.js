12c65879b6ffdd6267c25baf77f0cd7969839a19
Sam Kaminski <113308993+samkaminski@users.noreply.github.com>
2024-06-02 13:12:48 -0400

    Delete css directory

--- css/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 0) {
-        document.body.classList.add('scrolled');
-    } else {
-        document.body.classList.remove('scrolled');
-    }
-});
