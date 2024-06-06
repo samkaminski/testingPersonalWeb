1cb26e7ec327c42c755e29ff2b6f6c3d45f6be2e
Sam Kaminski <113308993+samkaminski@users.noreply.github.com>
2024-06-06 18:59:35 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 0) {
-        document.body.classList.add('scrolled');
-    } else {
-        document.body.classList.remove('scrolled');
-    }
-});
-
-console.log("JavaScript file loaded successfully."); // For debugging
